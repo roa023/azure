6baa33804969f644b364bf4e30b4f601be099b21
roa023 <48248164+roa023@users.noreply.github.com>
2019-03-05 15:28:37 +0100

    Update AddusersToGroupV2.ps1

--- AddusersToGroupV2.ps1
+++ AddusersToGroupV2.ps1
@@ -1,8 +1,4 @@
 
 mkdir "C:\temp3"
 
-REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 0 /f
-
 Add-LocalGroupMember -Group "Remote Desktop Users" -Member "Azure\Domain Users"
-
-REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v ConsentPromptBehaviorAdmin /t REG_DWORD /d 5 /f
